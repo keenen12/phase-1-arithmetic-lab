965b780aace66e1705edb56b17673320804e558d
Keenen Harral <kharral23@yahoo.com>
2022-11-10 00:39:17 -0800

    commit tree 9d393ac38ffc142f9ba57b50b50414cf788dae69

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+
+let num3 = 20;
+let num4 = 8;
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 3, 20);
+
